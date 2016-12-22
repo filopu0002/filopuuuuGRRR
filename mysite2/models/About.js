ad5f81d1758670369171174f573d503f6a83faa2
filopu0002 <sylvainyner@MacBook-Pro-de-Sylvain.local>
2016-12-22 20:02:58 +0100

    First Commit

---
+++ mysite2/models/About.js
@@ -0,0 +1,34 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * About Model
+ * ==========
+ */
+
+var About = new keystone.List('About', {
+	map: { name: 'title' },
+	autokey: { path: 'slug',
+            from: 'title',
+            unique: true },
+});
+
+About.add({
+	title: { type: String, required: true },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+	author: { type: Types.Relationship, ref: 'Admin', index: true },
+	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+	image: { type: Types.CloudinaryImage },
+	content: {
+		brief: { type: Types.Html, wysiwyg: true, height: 150 },
+		extended: { type: Types.Html, wysiwyg: true, height: 400 },
+	},
+	categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+});
+
+// Post.schema.virtual('content.full').get(function () {
+// 	return this.content.extended || this.content.brief;
+// });
+
+//Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
+About.register();
